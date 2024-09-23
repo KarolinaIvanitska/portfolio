5bfb6070aa876ae5802c82576b20dd7fd6d66904
Karolina_Zaiats <karolina.ivanitska@gmail.com>
2024-09-23 14:51:29 +0200

    Add styles to skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,39 +4,51 @@ const Skills = () => {
   return (
     <section className={css.section_skills}>
       <div className={css.skills_container}>
-        <h2>Skills</h2>
+        <h2 className={css.skills}> My skills</h2>
         <div className={css.skills_cards}>
           <div className={css.card}>
             <h3>HTML5</h3>
-            <p>
-              Advanced knowledge of semantic HTML, accessibility best practices,
-              and SEO optimization.
-            </p>
+            <div className={css.overlay}>
+              <p>
+                Advanced knowledge of semantic HTML, accessibility best
+                practices, and SEO optimization.
+              </p>
+            </div>
           </div>
           <div className={css.card}>
             <h3>CSS3</h3>
-            <p>Proficient in responsive design, Flexbox and CSS Grid.</p>
+            <div className={css.overlay}>
+              <p>Proficient in responsive design, Flexbox and CSS Grid.</p>
+            </div>
           </div>
           <div className={css.card}>
             <h3>JavaScript</h3>
-            <p>
-              Strong understanding of ES6+, DOM manipulation, and event-driven
-              programming.
-            </p>
+            <div className={css.overlay}>
+              <p>
+                Strong understanding of ES6+, DOM manipulation, and event-driven
+                programming.
+              </p>
+            </div>
           </div>
           <div className={css.card}>
             <h3>React</h3>
-            <p>
-              Experienced in building dynamic web applications using React hooks
-              and state management.
-            </p>
+            <div className={css.overlay}>
+              {' '}
+              <p>
+                Experienced in building dynamic web applications using React
+                hooks and state management.
+              </p>
+            </div>
           </div>
           <div className={css.card}>
             <h3>Git & GitHub</h3>
-            <p>
-              Comfortable with version control, branching, and collaboration
-              through GitHub.
-            </p>
+            <div className={css.overlay}>
+              {' '}
+              <p>
+                Comfortable with version control, branching, and collaboration
+                through GitHub.
+              </p>
+            </div>
           </div>
         </div>
       </div>
